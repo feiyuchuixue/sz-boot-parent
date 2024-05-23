4e0829a4d2dcc277f9317fd35381b7d78a7a20e2
INS6+ <feiyuchuixue@163.com>
2024-05-23 17:12:39 +0800

    优化：pom功能项区分，SpringBoot启动版本号维护

--- sz-service/sz-service-admin/src/main/java/com/sz/AdminApplication.java
+++ sz-service/sz-service-admin/src/main/java/com/sz/AdminApplication.java
@@ -1,5 +1,7 @@
 package com.sz;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -7,6 +9,21 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @SpringBootApplication
 @EnableAspectJAutoProxy
 public class AdminApplication {
+
+    @Value("${app.version}")
+    private String appVersion;
+
+    private static String version;
+
+    @PostConstruct
+    public void init() {
+        AdminApplication.version = appVersion;
+    }
+
+    public static String getVersion() {
+        return version;
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(AdminApplication.class, args);
@@ -16,7 +33,7 @@ public class AdminApplication {
                 "( (`\\] [_   ]|______|`'_\\ : / /'`\\' | [ `.-. .-. | [  | [ `.-. |\n" +
                 " `'.'.  .' /_        // | |,| \\__/  |  | | | | | |  | |  | | | |\n" +
                 "[\\__) )[_____]       \\'-;__/ '.__.;__][___||__||__][___][___||__]\n" +
-                "------------------https://szadmin.cn  (v0.6.0 Beta)-------------------"
+                "------------------https://szadmin.cn  (v" + getVersion() + ")-------------------"
         );
     }
 
