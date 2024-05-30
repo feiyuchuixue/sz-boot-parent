88eb85ec22d9ad62a4e8cb6a9246498e57c7b892
INS6+ <feiyuchuixue@163.com>
2024-05-30 22:03:09 +0800

    新增：发送socket（测试）升级公告接口

--- sz-common/sz-common-core/src/main/java/com/sz/core/common/enums/SocketChannelEnum.java
+++ sz-common/sz-common-core/src/main/java/com/sz/core/common/enums/SocketChannelEnum.java
@@ -12,7 +12,8 @@ public enum SocketChannelEnum {
     KICK_OFF("kick_off", "踢下线", "push"),
     SYNC_MENU("sync_menu", "同步菜单", "push"),
     SYNC_DICT("sync_dict", "同步字典", "push"),
-    SYNC_PERMISSIONS("sync_permissions", "同步permission权限", "push");
+    SYNC_PERMISSIONS("sync_permissions", "同步permission权限", "push"),
+    UPGRADE_CHANNEL("upgrade_channel", "升级通告", "push");
 
 
     /**
--- sz-service/sz-service-admin/src/main/java/com/sz/www/test/controller/TestController.java
+++ sz-service/sz-service-admin/src/main/java/com/sz/www/test/controller/TestController.java
@@ -2,10 +2,16 @@ package com.sz.www.test.controller;
 
 import com.sz.core.common.entity.ApiPageResult;
 import com.sz.core.common.entity.ApiResult;
+import com.sz.core.common.entity.SocketBean;
+import com.sz.core.common.entity.TransferMessage;
+import com.sz.core.common.enums.MessageTransferScopeEnum;
+import com.sz.core.common.enums.SocketChannelEnum;
+import com.sz.redis.WebsocketRedisService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,9 +27,30 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class TestController {
 
+    private final WebsocketRedisService websocketRedisService;
+
     @Operation(summary = "测试")
     @GetMapping
     public ApiResult<String> test() {
         return ApiPageResult.success("这是一条测试信息");
     }
+
+
+    @PostMapping
+    @Operation(summary = "发送升级公告（socket）")
+    public ApiResult sendUpgradeMsg() {
+        SocketBean bean = new SocketBean<>();
+        bean.setData("系统即将进行升级，预计需要几分钟时间。请您稍等片刻，感谢您的耐心等待");
+        bean.setChannel(SocketChannelEnum.UPGRADE_CHANNEL);
+        bean.setScope(MessageTransferScopeEnum.SOCKET_CLIENT);
+
+        TransferMessage msg = new TransferMessage();
+        msg.setMessage(bean);
+        msg.setFromUser("system");
+        msg.setToPushAll(true);
+        websocketRedisService.sendServiceToWs(msg);
+
+        return ApiResult.success();
+    }
+
 }
