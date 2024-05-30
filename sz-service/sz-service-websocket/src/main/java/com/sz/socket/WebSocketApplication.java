8640a57b59f83843ae5774f5a53df724204c13fc
INS6+ <feiyuchuixue@163.com>
2024-05-30 10:56:00 +0800

    优化：清理代码

--- sz-service/sz-service-websocket/src/main/java/com/sz/socket/WebSocketApplication.java
+++ sz-service/sz-service-websocket/src/main/java/com/sz/socket/WebSocketApplication.java
@@ -1,7 +1,5 @@
 package com.sz.socket;
 
-import com.sz.socket.sever.WebSocketServer;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -9,10 +7,6 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan("com.sz")
 public class WebSocketApplication {
-
-    @Autowired
-    private WebSocketServer webSocketServer;
-
     public static void main(String[] args) {
         SpringApplication.run(WebSocketApplication.class, args);
     }
