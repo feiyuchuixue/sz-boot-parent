ee4c60e9d16f55e7954056a0c6c8a9465249e24b
INS6+ <feiyuchuixue@163.com>
2024-06-01 13:10:25 +0800

    优化：websocket服务scan目录

--- sz-service/sz-service-websocket/src/main/java/com/sz/socket/WebSocketApplication.java
+++ sz-service/sz-service-websocket/src/main/java/com/sz/socket/WebSocketApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.sz")
+@ComponentScan("com.sz.redis")
 public class WebSocketApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebSocketApplication.class, args);
