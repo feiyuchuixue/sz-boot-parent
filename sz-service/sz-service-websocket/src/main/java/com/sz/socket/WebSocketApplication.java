9f60d350043b46e6895b21b36147873e852e872f
INS6+ <feiyuchuixue@163.com>
2024-10-28 16:17:01 +0800

    优化：sz-service-socket添加启动Banner

--- sz-service/sz-service-websocket/src/main/java/com/sz/socket/WebSocketApplication.java
+++ sz-service/sz-service-websocket/src/main/java/com/sz/socket/WebSocketApplication.java
@@ -1,5 +1,8 @@
 package com.sz.socket;
 
+import jakarta.annotation.PostConstruct;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -8,8 +11,26 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan("com.sz")
 public class WebSocketApplication {
 
+    @Value("${app.version}")
+    private String appVersion;
+
+    @Getter
+    private static String version;
+
+    @PostConstruct
+    public void init() {
+        version = appVersion;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(WebSocketApplication.class, args);
+        System.out.println("                                           __             _    \n" +
+                "                                          [  |  _        / |_  \n" +
+                " .--.   ____  ______  .--.   .--.   .---.  | | / ] .---.`| |-' \n" +
+                "( (`\\] [_   ]|______|( (`\\]/ .'`\\ \\/ /'`\\] | '' < / /__\\\\| |   \n" +
+                " `'.'.  .' /_         `'.'.| \\__. || \\__.  | |`\\ \\| \\__.,| |,  \n" +
+                "[\\__) )[_____]       [\\__) )'.__.' '.___.'[__|  \\_]'.__.'\\__/  \n" + "------------------https://szadmin.cn  (v" + getVersion()
+                + ")-------------------");
     }
 
 }
