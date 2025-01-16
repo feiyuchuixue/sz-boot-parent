6c7ec45d738db2b1cbd8cd98cdf41a29bbfea79b
Think-PC <admin@example.com>
2025-01-16 14:33:40 +0800

    新增: 1. sz-dependencies 增加xxl-job版本引用 2. 增加sz-service-xxl-job

---
+++ sz-service/sz-service-xxl-job/README.md
@@ -0,0 +1,4 @@
+服务端启动(默认端口9820): java -jar xxl-job-admin-2.4.2.jar
+服务端地址: http://127.0.0.1:9820/xxl-job-admin
+账号: admin
+密码: 123456
---
+++ sz-service/sz-service-xxl-job/src/main/java/com/sz/xxl/job/executor/XxlJobExecutorApplication.java
@@ -0,0 +1,11 @@
+package com.sz.xxl.job.executor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XxlJobExecutorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(XxlJobExecutorApplication.class, args);
+    }
+}
