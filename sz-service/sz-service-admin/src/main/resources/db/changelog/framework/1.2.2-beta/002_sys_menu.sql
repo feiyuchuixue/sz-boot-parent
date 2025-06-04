ad1d297e4fb25f14241d213d2d57e7232128aa3a
Kang-Yang <591616450@qq.com>
2025-06-04 16:23:03 +0800

    fix: 修正菜单路径和组件路径拼写错误

--- sz-service/sz-service-admin/src/main/resources/db/changelog/framework/1.2.2-beta/002_sys_menu.sql
+++ sz-service/sz-service-admin/src/main/resources/db/changelog/framework/1.2.2-beta/002_sys_menu.sql
@@ -1,5 +1,6 @@
 --liquibase formatted sql
 
---changeset 升职哦（sz）:20250530_1125
+--changeset 升职哦（sz）:20250604_1621
 --comment: 修正菜单路径拼写错误
 UPDATE `sys_menu` SET `path` = '/system/menuManage' WHERE `path` = '/system/menuMange';
+UPDATE `sys_menu` SET `component` = '/system/menuManage/index' WHERE `component` = '/system/menuMange/index';
