b35c56ce936e197e60c0b13d7cf229572c26d47c
INS6+ <feiyuchuixue@163.com>
2026-01-16 22:22:53 +0800

    修改：设置三个系统参数为锁定状态

--- sz-service/sz-service-admin/src/main/resources/db/changelog/framework/1.3.2-beta/002_system.sql
+++ sz-service/sz-service-admin/src/main/resources/db/changelog/framework/1.3.2-beta/002_system.sql
@@ -5,3 +5,8 @@ ALTER TABLE `sys_config` ADD COLUMN `frontend_visible` enum('T','F') CHARACTER S
 
 --changeset 升职哦（sz）:20260116_2025
 INSERT INTO `sys_config` (`config_name`, `config_key`, `config_value`, `is_lock`, `frontend_visible`, `create_id`, `create_time`, `update_id`, `update_time`, `remark`) VALUES ('资源访问模式', 'oss.accessMode', 'private', 'F', 'T', 1, '2026-01-13 09:59:09', 1, '2026-01-16 13:57:52', '用于控制前端资源文件（如 Minio OSS）访问方式。当值为 public 时，前端直接使用数据库中存储的完整资源 URL 进行加载，适用于公开访问的资源；当值为 private 时，前端通过 Minio 获取私有资源的临时授权地址进行加载，适用于需权限验证的私有资源。');
+
+--changeset 升职哦（sz）:20260116_2215
+UPDATE `sys_config` SET `is_lock` = 'T' WHERE `config_key` = 'oss.accessMode';
+UPDATE `sys_config` SET `is_lock` = 'T' WHERE `config_key` = 'sys.login.requestCycle';
+UPDATE `sys_config` SET `is_lock` = 'T' WHERE `config_key` = 'sys.login.requestLimit';
